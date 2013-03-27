46e7f89b38e8b1f5a6b7057240051b6ec71ef796
Fengwei Yin <fengwei.yin@intel.com>
2013-03-27 17:54:28 +0800

    [PORT FROM MAIN] build: enable FORTIFY_SOURCE for android x86 build
    BZ: 96135
    
    Add option to enable FORTIFY_SOURCE for android x86.
    
    Change-Id: I1d244acf910ed738b16fe9a6608563a9cec682ea
    Orig-Change-Id: If3edf47f1569495fd0c7938bc58b04c42ffe7b11
    Reviewed-on: http://android.intel.com:8080/123569
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: Monnier, Olivier <olivier.monnier@intel.com>
    Reviewed-by: Monnier, Olivier <olivier.monnier@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- core/combo/TARGET_linux-x86.mk
+++ core/combo/TARGET_linux-x86.mk
@@ -101,6 +101,7 @@ TARGET_GLOBAL_CFLAGS += \
 			-ffunction-sections \
 			-finline-functions \
 			-finline-limit=300 \
+			-D_FORTIFY_SOURCE=1 \
 			-fno-inline-functions-called-once \
 			-fno-short-enums \
 			-fstrict-aliasing \
