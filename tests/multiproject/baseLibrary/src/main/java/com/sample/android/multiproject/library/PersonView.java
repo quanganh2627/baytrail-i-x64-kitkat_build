06555c7a2c7593d1915dc29d0b2b8984b99d4ff4
Siva Velusamy <vsiva@google.com>
2013-11-05 16:46:57 -0800

    Fix import paths in tests so that they work in the IDE
    Change-Id: Ie5cdb99025466f1bd50ec6aee793aa92f4c5c5de

--- tests/multiproject/baseLibrary/src/main/java/com/sample/android/multiproject/library/PersonView.java
+++ tests/multiproject/baseLibrary/src/main/java/com/sample/android/multiproject/library/PersonView.java
@@ -1,10 +1,10 @@
-package com.example.android.multiproject.library;
+package com.sample.android.multiproject.library;
 
 import android.widget.TextView;
 import android.content.Context;
 import com.example.android.multiproject.person.Person;
 
-class PersonView extends TextView {
+public class PersonView extends TextView {
     public PersonView(Context context, Person person) {
         super(context);
         setTextSize(20);
