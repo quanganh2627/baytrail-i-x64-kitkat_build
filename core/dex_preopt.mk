df44d2ca05319dc41b8865784fa42009ca83204f
charitardha <satya.charitardha.jayanti@intel.com>
2013-12-23 15:17:07 +0530

    [Dolby DS1] Build compilation halts when DS1 is enabled
    BZ: 160735
    
    The dolby jar is already included in BOOTCLASSPATH in
    device/intel/common/BoardConfig.mk.
    This patch removes the duplicate inclusion of the jar
    in dex_preopt.mk
    
    Change-Id: Ib1f83a532e2ccd23b374edb53875c701d6d94c81
    Category: feature differentiation
    Domain: AUDIO_MEDIA-Dolby
    Origin: 3rd-party Dolby
    Upstream-Candidate: no, proprietary
    Signed-off-by: charitardha <satya.charitardha.jayanti@intel.com>

--- core/dex_preopt.mk
+++ core/dex_preopt.mk
@@ -1,31 +1,9 @@
-#
-# This file was modified by Dolby Laboratories, Inc. The portions of the
-# code that are surrounded by "DOLBY..." are copyrighted and
-# licensed separately, as follows:
-#
-#  (C) 2012-2013 Dolby Laboratories, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 ####################################
 # Dexpreopt on the boot jars
 #
 ####################################
 
 DEXPREOPT_BOOT_JARS := $(PRODUCT_BOOT_JARS)
-ifdef DOLBY_DAP
-DEXPREOPT_BOOT_JARS:=$(DEXPREOPT_BOOT_JARS):dolby_ds
-endif
 DEXPREOPT_BOOT_JARS_MODULES := $(subst :, ,$(DEXPREOPT_BOOT_JARS))
 PRODUCT_BOOTCLASSPATH := $(subst $(space),:,$(foreach m,$(DEXPREOPT_BOOT_JARS_MODULES),/system/framework/$(m).jar))
 
