7deeaadaf18a951d4f020abaeeb5abb3c109c141
Laurent FERT <laurent.fert@intel.com>
2014-01-16 20:01:45 +0100

    Prebuilts: No dex preopt on static javalib
    BZ: 165135
    
    Prebuilt static java libraries should be uninstallable and do not
    requite dex optimizations.
    
    Check for uninstallable flag to avoid dex optimizations.
    Fix redefintion of build recipe for built module.
    
    Change-Id: Icff566f21e6c2b9f496587d9a00cda12e7c2596f
    Category: aosp improvement
    Domain: AOSP-Framework-CoreService
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Laurent FERT <laurent.fert@intel.com>

--- core/prebuilt.mk
+++ core/prebuilt.mk
@@ -197,7 +197,7 @@ ifeq ($(LOCAL_DEX_PREOPT),true)
 built_odex := $(basename $(built_module)).odex
 $(built_odex): $(built_module)
 endif
-else
+else ifneq ($(LOCAL_IS_HOST_MODULE)$(LOCAL_MODULE_CLASS),JAVA_LIBRARIES)
 ifneq ($(LOCAL_PREBUILT_STRIP_COMMENTS),)
 $(built_module) : $(my_prebuilt_src_file)
 	$(transform-prebuilt-to-target-strip-comments)
@@ -236,7 +236,10 @@ $(common_javalib_jar) : $(my_prebuilt_src_file) | $(ACP)
 
 # make sure the classes.jar and javalib.jar are built before $(LOCAL_BUILT_MODULE)
 $(built_module) : $(common_javalib_jar)
+	$(call copy-file-to-target)
 
+# Static libraries should be uninstallable and are not optimized.
+ifneq ($(LOCAL_UNINSTALLABLE_MODULE),true)
 # Do pre-optimization of the libraries according to build settings.
 # Use the same procedure as java_library.mk for compiled modules.
 ifeq ($(LOCAL_DEX_PREOPT),true)
@@ -267,4 +270,5 @@ $(LOCAL_BUILT_MODULE) : $(common_javalib_jar) | $(AAPT) $(ACP)
 	$(call dexpreopt-remove-classes.dex,$@)
 endif # dexpreopt_boot_jar_module
 endif # LOCAL_DEX_PREOPT
+endif # LOCAL_UNINSTALLABLE_MODULE
 endif # TARGET JAVA_LIBRARIES
