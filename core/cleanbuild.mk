dfc45ac0a64a3e6f08aa3ad993d61682a6d2d692
rdoddix <ravindranathx.doddi@intel.com>
2014-01-23 18:47:14 -0800

    build: Fix to include userfastboot instead of droidboot
    The latest IAGO uses userfastboot instead of droidboot.
    This requires the naming convention change in all
    the places droidboot has been referred to.
    
    Change-Id: Ie07f941da69f20655dd196a5387897ee3be29198
    Signed-off-by: Ravindranath D <ravindranathx.doddi@intel.com>

--- core/cleanbuild.mk
+++ core/cleanbuild.mk
@@ -195,7 +195,7 @@ installclean_files := \
 	$(PRODUCT_OUT)/obj/JAVA_LIBRARIES \
 	$(PRODUCT_OUT)/iago \
 	$(PRODUCT_OUT)/efi \
-	$(PRODUCT_OUT)/droidboot \
+	$(PRODUCT_OUT)/userfastboot \
 	$(PRODUCT_OUT)/obj/FAKE \
 	$(PRODUCT_OUT)/obj/EXECUTABLES/adbd_intermediates \
 	$(PRODUCT_OUT)/obj/EXECUTABLES/init_intermediates \
